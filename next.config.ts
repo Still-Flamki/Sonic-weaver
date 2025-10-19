8caf633ffc31c839b446472969b38dde5ae90c30
bleu .x <bleu.x04@gmail.com>
2025-10-19 17:46:51 +0000

    /node_modules/@ffmpeg/ffmpeg/dist/esm/classes.js (105:28)
    Module not fo

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,34 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    // See https://github.com/ffmpegwasm/ffmpeg.wasm/issues/738
+    if (!isServer) {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            "fs": false,
+            "path": false
+        }
+    }
+    config.module.rules.push({
+      test: /node_modules\/@ffmpeg\/ffmpeg\/dist\/esm\/worker\.js$/,
+      loader: 'string-replace-loader',
+      options: {
+        search: 'import.meta.url',
+        replace: 'self.location.href',
+      },
+    });
+    config.module.rules.push({
+      test: /node_modules\/@ffmpeg\/ffmpeg\/dist\/esm\/classes\.js$/,
+      loader: 'string-replace-loader',
+      options: {
+        search: 'import.meta.url',
+        replace: 'self.location.href',
+      },
+    });
+     config.output.publicPath = '/_next/';
+    return config
+  },
 };
 
 export default nextConfig;
