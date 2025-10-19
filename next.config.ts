d5b55c2d0b0018bf2cf7319357cf2a55babda110
bleu .x <bleu.x04@gmail.com>
2025-10-19 11:30:05 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // This is required to allow the Next.js dev server to accept requests from
-    // the Firebase Studio development environment.
-    allowedDevOrigins: [
-      '*.cloudworkstations.dev',
-      '*.apphosting.dev',
-    ],
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
